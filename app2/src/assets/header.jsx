569bf48d0c65aef7a307e487856bf52b254e48db
UlquiorraCiferR4 <skanderbouafif8@gmail.com>
2026-02-25 16:58:32 +0100

    update

---
+++ app2/src/assets/header.jsx
@@ -0,0 +1,14 @@
+import './nav.css'
+
+export function Header() {
+  return (
+    <nav className="navBar">
+      <div className="part1">
+        
+      </div>
+      <<div className="part2">
+                
+      </div>
+    </nav> 
+  ); 
+}
