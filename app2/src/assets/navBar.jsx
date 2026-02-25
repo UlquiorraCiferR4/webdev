4a046afb0c1450386db1014de22f974a4cf5933c
UlquiorraCiferR4 <skanderbouafif8@gmail.com>
2026-02-25 17:01:51 +0100

    update

---
+++ app2/src/assets/navBar.jsx
@@ -0,0 +1,11 @@
+export function Nav() {
+  return (
+    <ul>
+
+      <li><a href="#">link</a></li>  
+      <li><a href="#">link</a></li>  
+      <li><a href="#">link</a></li>  
+
+    </ul>
+  )
+}
